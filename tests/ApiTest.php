55b56afc6e3c29822904d0c83511a0d8513c8b0a
Kaishiyoku <dmp@nfze.de>
2020-09-15 21:45:11 +0200

    added Guzzle 7 support, added phpunit.xml

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,11 +1,15 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
 class ApiTest extends TestCase
 {
     public $flickr;
 
     public function __construct()
     {
+        parent::__construct();
+
         $api = new JeroenG\Flickr\Api($_ENV['FLICKR_KEY'], 'php_serial');
         $this->flickr = new JeroenG\Flickr\Flickr($api);
     }
